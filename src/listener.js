be14c4f9c1f10eb3cb4877b78f1298aa24450afb
steven-fo <stevenfo0711@gmail.com>
2023-08-05 16:20:58 +0700

    bind fucntion XD

--- src/listener.js
+++ src/listener.js
@@ -3,6 +3,8 @@ class Listener {
   constructor(playlistsService, mailSender) {
     this._playlistsService = playlistsService;
     this._mailSender = mailSender;
+
+    this.listen = this.listen.bind(this);
   }
 
   async listen(message) {
